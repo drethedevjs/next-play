af70318a25054f6517dbb5ce310cd505d5f3df6e
Andre Thomas <drethedevjs@gmail.com>
2024-11-20 16:45:28 -0500

    Faded image and styling updates.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -8,21 +8,22 @@ const headshotUrl = ref(headshot);
 </script>
 
 <template>
-  <div class="lg:flex lg:flex-row">
+  <div class="lg:flex lg:flex-row text-dark">
     <div class="mobile-image-pane">
       <img :src="headshotUrl" class="object-cover w-full" />
     </div>
     <div class="action-side">
       <h1><span>Dr.</span><br>Deverin Muff</h1>
-      <p class="tag-line">Speaker. Author. Coach</p>
-      <p class="mission-line">We exist to help athletes find their the <span class="text-primary">next play</span> during their life after sports.</p>
+      <p class="tag-line">Speaker. Author. Coach. <span class="christmas">Grinch</span>.</p>
+      <p class="mission-line">We exist to help athletes find their the <span class="text-secondary">next play</span> during their life after sports.</p>
       <div class="action-btns">
         <button class="book-me-btn">Book Me</button>
         <button class="subscribe-btn">Subscribe to Newsletter</button>
       </div>
     </div>
-    <div class="image-side">
+    <div class="image-side relative">
       <img :src="headshotUrl" class="object-cover h-full w-full" />
+      <div class="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-b from-transparent to-white"></div>
     </div>
   </div>
 </template>
@@ -60,10 +61,14 @@ const headshotUrl = ref(headshot);
   }
 
   .book-me-btn {
-    @apply py-6 px-9 border-2 border-primary rounded-md bg-primary font-bold mr-5 hover:bg-white  transition-colors;
+    @apply py-6 px-9 border-2 border-primary rounded-md bg-primary font-bold mr-5 hover:bg-white transition-colors;
   }
 
   .subscribe-btn {
     @apply py-6 px-9 border-2 border-primary rounded-md font-bold hover:bg-primary hover:text-white transition-colors;
+  }
+
+  .christmas {
+    @apply text-transparent bg-clip-text bg-gradient-to-r from-green-800 to-green-400 font-extrabold;
   }
 </style>
