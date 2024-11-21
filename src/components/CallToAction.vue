ee0c92be203ae0f8b7f099a55996e4a1caa3cde1
Andre Thomas <drethedevjs@gmail.com>
2024-11-21 11:26:50 -0500

    Added call to actions component and added it to the home page.

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div id="cta-container">
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <slot name="body"></slot>
+      <slot name="action"></slot>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#cta-container {
+  @apply lg:py-24 py-24 sm:px-20 px-8 text-center;
+}
+
+p {
+  @apply text-3xl text-white mb-10
+}
+
+.btn {
+  @apply bg-secondary hover:bg-transparent text-white border-2 border-secondary transition-colors py-3 px-5 mb-3 rounded font-bold;
+}
+
+.spec-container {
+    @apply lg:py-52 py-24 sm:px-20 px-8;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import CallToAction from '../components/CallToAction.vue';
 import Hero from '../components/Hero.vue';
 import Specialties from '../components/Specialties.vue';
 </script>
@@ -6,4 +7,51 @@ import Specialties from '../components/Specialties.vue';
 <template>
   <Hero />
   <Specialties />
+  <CallToAction class="bg-dark">
+    <template #header>
+      <h1 class="pane-header">Ready to Start?</h1>
+    </template>
+    <template #body>
+      <p class="text-white">Take the first step toward transforming your athletic journey. 
+        Whether you're an athlete redefining your purpose, a parent seeking guidance, 
+        or a coach shaping tomorrow's leaders, we're here to help.
+      </p>
+    </template>
+    <template #action>
+      <button class="btn ready-btn">Book You Free Discovery Call</button>
+    </template>
+  </CallToAction>
+
+  <CallToAction>
+    <template #header>
+      <h1 class="pane-header">Got Questions</h1>
+    </template>
+    <template #body>
+      <p class="text-dark">
+        Check out our <span class="text-secondary">FAQ</span> page or reach out via our <span class="text-secondary">Contact Us</span> page for more details.
+      </p>
+    </template>
+    <template #action>
+      <button class="btn action-btn mr-3">FAQ</button>
+      <button class="btn action-btn">Contact Us</button>
+    </template>
+  </CallToAction>
 </template>
+
+<style scoped>
+  p {
+    @apply text-3xl mb-10
+  }
+
+  .btn {
+    @apply transition-colors sm:py-3 sm:px-5 sm:text-base py-5 px-7 text-3xl mb-3 rounded font-bold hover:bg-transparent;
+  }
+
+  .ready-btn {
+    @apply bg-secondary text-white border-2 border-secondary;
+  }
+
+  .action-btn {
+    @apply bg-secondary text-white hover:text-dark border-2 border-secondary;
+  }
+</style>
