efbb962688f7993ce250a08b487167942e6ec8f0
Andre Thomas <drethedevjs@gmail.com>
2024-11-22 14:37:56 -0500

    Added Vue DevTools as a plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import vue from '@vitejs/plugin-vue';
 import path from "path";
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   base: '/',
   resolve: {
     alias: {
