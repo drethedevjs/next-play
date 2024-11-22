83507e3071bbb3a257a46672338ed455752834c8
Andre Thomas <drethedevjs@gmail.com>
2024-11-22 12:16:00 -0500

    Removed unused styling.

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -14,16 +14,4 @@
 #cta-container {
   @apply lg:py-24 py-24 sm:px-20 px-8 text-center 2xl:w-2/3 xl:w-1/2 w-full mx-auto;
 }
-
-p {
-  @apply text-3xl text-white mb-10
-}
-
-.btn {
-  @apply bg-secondary hover:bg-transparent text-white border-2 border-secondary transition-colors py-3 px-5 mb-3 rounded font-bold;
-}
-
-.spec-container {
-    @apply lg:py-52 py-24 sm:px-20 px-8;
-  }
 </style>
