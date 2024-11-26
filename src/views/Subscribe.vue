291190450fe6a6df8280e5ddc9d34259e45270b0
Andre Thomas <drethedevjs@gmail.com>
2024-11-25 21:23:54 -0500

    Fixed subscribe page on mobile.

--- src/views/Subscribe.vue
+++ src/views/Subscribe.vue
@@ -1,23 +1,23 @@
 <template>
 <h1>Subscribe</h1>
-<div class="container mx-auto px-56 iframe-container">
+<div class="iframe-container">
   <iframe 
-  src="https://deverinmuff.substack.com/embed" 
-  frameborder="0">
-</iframe>
+    src="https://deverinmuff.substack.com/embed" 
+    frameborder="0">
+  </iframe>
 </div>
 </template>
 
 <style scoped>
-h1{
+h1 {
   @apply text-center sm:text-8xl text-5xl font-bold uppercase border-b-2 border-b-dark pb-3 mb-10;
 }
 
 iframe {
-  @apply min-h-full w-full;
+  @apply sm:min-h-full sm:w-full h-96 mx-auto;
 }
 
 .iframe-container {
-  @apply h-96;
+  @apply sm:container sm:mx-auto sm:px-56 sm:h-96;
 }
 </style>
