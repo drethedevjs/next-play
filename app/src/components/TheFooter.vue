ca9b6749d91078e792c07f53a4ae8a8d8fc75c63
Andre Thomas <drethedevjs@gmail.com>
2024-12-04 13:41:58 -0500

    Added "Power by" to footer.

--- app/src/components/TheFooter.vue
+++ app/src/components/TheFooter.vue
@@ -1,11 +1,16 @@
 <template>
   <footer role="contentinfo">
-    Copyright <span class="text-secondary">Next Play</span> 2024
+    <div>Copyright <span class="text-secondary">Next Play</span> 2024</div>
+    <div class="mt-5 scale-75">Powered By 
+      <span class="text-secondary hover:text-eggshell">
+        <a href="https://csra-web-services.vercel.app/" target="_blank">CSRA Web Dev Services</a>
+      </span>
+    </div>
   </footer>
 </template>
 
 <style scope>
 footer {
-  @apply bg-dark h-24 text-white font-semibold place-content-center text-center;
+  @apply bg-dark h-32 text-white font-semibold place-content-center text-center;
 }
 </style>
