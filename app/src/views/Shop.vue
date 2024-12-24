b9510a946e771f06a12e8e4b4b8f925f6506973a
Andre Thomas <drethedevjs@gmail.com>
2024-12-24 14:29:02 -0500

    Removed unnecessary class.

--- app/src/views/Shop.vue
+++ app/src/views/Shop.vue
@@ -2,7 +2,7 @@
   <div class="np-container">
     <h1 class="page-h1">Shop</h1>
     <div class="place-items-center mb-10">
-      <div class="payhip-embed-page npBorder" data-key="R1IbG"></div>
+      <div class="payhip-embed-page" data-key="R1IbG"></div>
     </div>
   </div>
 </template>
