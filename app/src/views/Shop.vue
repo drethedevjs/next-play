ebc501921a7331e34ac2ed5e8e2c90f128b74c53
Andre Thomas <drethedevjs@gmail.com>
2024-12-28 17:08:38 -0500

    Fixed shop embed issue.

--- app/src/views/Shop.vue
+++ app/src/views/Shop.vue
@@ -1,7 +1,21 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+
+const initializePayhipEmbed = () => {
+  const existingScript = document.querySelector('script[src="https://payhip.com/embed-page.js?v=24u68984"]');
+    if (existingScript) {
+      // If the script already exists, re-trigger it
+      (window as any).PayhipEmbedPage?.init();
+    }
+};
+
+onMounted(() => initializePayhipEmbed());
+
+</script>
 <template>
   <div class="np-container">
     <h1 class="page-h1">Shop</h1>
-    <div class="place-items-center mb-10">
+    <div class="place-items-center mb-10" id="payhip-embed-container">
       <div class="payhip-embed-page" data-key="R1IbG"></div>
     </div>
   </div>
