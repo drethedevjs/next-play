0ea3151869cf278d69558147768145158632d293
Andre Thomas <drethedevjs@gmail.com>
2026-01-24 21:12:46 -0500

    Updated copyright dates and powered by name in footer.

--- app/src/components/TheFooter.vue
+++ app/src/components/TheFooter.vue
@@ -1,9 +1,17 @@
+<script lang="ts" setup>
+const currentYear: number = new Date().getFullYear();
+</script>
+
 <template>
   <footer role="contentinfo">
-    <div>Copyright <span class="text-secondary">Next Play</span> 2024</div>
-    <div class="mt-5 scale-75">Powered By 
+    <div>
+      Copyright <span class="text-secondary">Next Play</span> 2024 -
+      {{ currentYear }}
+    </div>
+    <div class="mt-5 scale-75">
+      Powered By
       <span class="text-secondary hover:text-cream">
-        <a href="https://csra-web-services.vercel.app/" target="_blank">CSRA Web Dev Services</a>
+        <a href="https://honeystack.solutions" target="_blank">Honeystack</a>
       </span>
     </div>
   </footer>
